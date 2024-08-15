1893ae2af90e8fb6d103b44687b321363172e18c
Pong <samfunghp@gmail.com>
2024-08-15 15:46:33 +0100

    fix(): build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,24 @@
+import fs from 'fs';
 import path from 'path';
 import dts from 'vite-plugin-dts';
 import { defineConfig } from 'vite';
+import { builtinModules } from 'module';
 
 const formats = ['es'] as const;
+
+const deps = [path.join(__dirname, 'package.json')].reduce((deps, pathname) => {
+  if (pathname.endsWith('package.json')) {
+    const pkg = JSON.parse(fs.readFileSync(pathname, 'utf-8')) as Record<string, unknown>;
+    return [
+      ...deps,
+      ...Object.keys(pkg.dependencies || {}),
+      ...Object.keys(pkg.devDependencies || {}),
+      ...Object.keys(pkg.peerDependencies || {}),
+      ...Object.keys(pkg.optionalDependencies || {})
+    ];
+  }
+  return deps;
+}, [] as string[]);
 
 export default defineConfig({
   plugins: [dts({ exclude: ['**/*.test.ts'] })],
@@ -20,7 +36,14 @@ export default defineConfig({
         format,
         entryFileNames: `[name].${format === 'es' ? 'js' : 'cjs'}`
       })),
-      external: [/node_modules/, 'valtio', /valtio\/.*/]
+      external: [
+        /node_modules/,
+        'react/jsx-runtime',
+        'protobufjs/minimal',
+        ...builtinModules,
+        ...builtinModules.map(m => `node:${m}`),
+        ...Array.from(new Set(deps), d => new RegExp(`^${d}`))
+      ]
     }
   }
 });
